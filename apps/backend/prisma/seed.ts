7f6b7f52a96da50453f02145c6c5f901c278726c
Kevin Ruhl <kevin.ruhl92@gmail.com>
2024-05-30 01:25:25 -0300

    defines seed file to intially seed the db with 10 pokemon; adds seed command

---
+++ apps/backend/prisma/seed.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import pokemon from './seed.json';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const existingPokemon = await prisma.pokemon.findFirst();
+
+  if (existingPokemon) {
+    console.log('Data already exists in the database. Skipping seed.');
+    return;
+  }
+
+  pokemon.forEach(async (p: any) => {
+    await prisma.pokemon.create({
+      data: {
+        name: p.name,
+        type: p.type,
+        hp: p.hp,
+        attack: p.attack,
+        description: p.description,
+        weakness: p.weakness,
+        resistance: p.resistance,
+      },
+    });
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
