fb6b537d8b33a6ce8d485943408a9084876aea0e
Kevin Ruhl <kevin.ruhl92@gmail.com>
2024-05-31 13:57:30 -0300

    refacotring changes

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,8 +1,5 @@
-import Link from "next/link";
-import { Pokemon } from "@/types/pokemon.types";
-import { PokemonCard } from "./components/PokemonCard";
 import { Metadata } from "next";
-import { PokemonComposer } from "./components/PokemonComposer";
+import { MainSection } from "./components/MainSection";
 
 export const metadata: Metadata = {
   title: "Pokemon Battle Simulator | Home",
@@ -10,28 +7,10 @@ export const metadata: Metadata = {
 };
 
 export default async function HomePage() {
-  const allPokemon: Pokemon[] = await fetch(`${process.env.API_URL}/pokemon`, {
-    cache: "no-cache",
-  }).then((res) => res.json());
-
   return (
     <main className="flex justify-center items-center w-100 my-20 px-20">
       <div id="modal-root"></div>
-      <div>
-        <div className="flex justify-between items-center">
-          <h1 className="text-4xl font-bold text-cyan-900 mb-10">
-            Pokemon Battle Simulator
-          </h1>
-          <PokemonComposer />
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {allPokemon.map((pokemon) => (
-            <Link href={`/pokemon/${pokemon.id}`} key={pokemon.id}>
-              <PokemonCard pokemon={pokemon} />
-            </Link>
-          ))}
-        </div>
-      </div>
+      <MainSection />
     </main>
   );
 }
